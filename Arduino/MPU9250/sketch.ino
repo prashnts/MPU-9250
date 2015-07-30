7e217fb9d24192c1ce98d0911e68a54c4553bb81
Prashant Sinha <prashant@ducic.ac.in>
2015-07-30 15:02:41 +0530

    Removal

--- Arduino/MPU9250/sketch.ino
+++ Arduino/MPU9250/sketch.ino
@@ -13,12 +13,10 @@ void setup() {
 
   // Read the WHO_AM_I register, this is a good test of communication
   byte c = readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX); Serial.print(" I should be "); Serial.println(0x71, HEX);
   
   delay(1000);
 
   if (c == 0x71) {
-
     Serial.println("MPU9250 is online");
     
     MPU9250SelfTest(SelfTest); // Start by performing self test and reporting values
@@ -28,13 +26,14 @@ void setup() {
     Serial.print("x-axis self test: gyration trim within : "); Serial.print(SelfTest[3],1); Serial.println("% of factory value");
     Serial.print("y-axis self test: gyration trim within : "); Serial.print(SelfTest[4],1); Serial.println("% of factory value");
     Serial.print("z-axis self test: gyration trim within : "); Serial.print(SelfTest[5],1); Serial.println("% of factory value");
-    
-    calibrateMPU9250(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
+
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    calibrateMPU9250(gyroBias, accelBias);
     
     delay(1000);
     
     initMPU9250();
-    Serial.println("MPU9250 initialized for active data mode"); // Initialize device for active mode read of acclerometer, gyroscope, and temperature
+    Serial.println("MPU9250 initialized for active data mode");
     
     // Read the WHO_AM_I register of the magnetometer, this is a good test of communication
     byte d = readByte(AK8963_ADDRESS, WHO_AM_I_AK8963); // Read WHO_AM_I register for AK8963
@@ -46,7 +45,6 @@ void setup() {
     initAK8963(magCalibration); Serial.println("AK8963 initialized for active data mode....");
 
     if(SerialDebug) {
-      // Serial.println("Calibration values: ");
       Serial.print("X-Axis sensitivity adjustment value "); Serial.println(magCalibration[0], 2);
       Serial.print("Y-Axis sensitivity adjustment value "); Serial.println(magCalibration[1], 2);
       Serial.print("Z-Axis sensitivity adjustment value "); Serial.println(magCalibration[2], 2); 
