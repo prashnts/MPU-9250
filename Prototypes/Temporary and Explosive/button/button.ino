4ecbfc62e8f6fee3f28476317d36b7bc5e296f04
Prashant Sinha <prashant@ducic.ac.in>
2015-08-12 20:24:51 +0530

    Finished up the Button Reads

---
+++ Prototypes/Temporary and Explosive/button/button.ino
@@ -0,0 +1,46 @@
+void setup() {
+  init_dip_switch();
+  Serial.begin(9600);
+  Serial.println("LOL");
+}
+
+void loop() {
+  Serial.print("Motion Class: ");
+  Serial.print(read_motion_class());
+  Serial.print(", Logger Location: ");
+  Serial.print(read_logger_location());
+  Serial.print(", SD Active: ");
+  Serial.println(read_sd_active()); 
+}
+
+void init_dip_switch() {
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  //pinMode(9, INPUT);
+}
+
+int read_motion_class() {
+  int out = 0x0000;
+  for (int i = 5; i >= 2; i--) {
+    out = (out << 1) | digitalRead(i);
+  }
+  return out;
+}
+
+bool read_sd_active() {
+  return (bool) digitalRead(8);
+}
+
+int read_logger_location() {
+  int out = 0x00;
+  for (int i = 7; i >= 6; i--) {
+    out = (out << 1) | digitalRead(i);
+  }
+  return out;
+}
+
