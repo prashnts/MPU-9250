ac57192c22875affad054e239b50aa60194dfbbd
Prashant Sinha <prashant@ducic.ac.in>
2015-08-19 15:45:23 +0530

    Added the PC project

--- Point-Cloud/test.py
+++ Point-Cloud/test.py
@@ -3,3 +3,7 @@
 
 import scipy.io as scio
 
+a = scio.loadmat("tof-confidence-data/training-scenes/room/room_pmd_data.mat")
+
+print(len(a['room_pmd_data'][0][0][0][199]))
+print(a['room_pmd_data'][0][0][0][199])
