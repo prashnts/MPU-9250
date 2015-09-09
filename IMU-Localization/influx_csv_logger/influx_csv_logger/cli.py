8a67200491ded9145a0095e24848bae960f48095
Prashant Sinha <prashant@ducic.ac.in>
2015-09-09 12:56:13 +0530

    Feature!

--- IMU-Localization/influx_csv_logger/influx_csv_logger/cli.py
+++ IMU-Localization/influx_csv_logger/influx_csv_logger/cli.py
@@ -366,6 +366,73 @@ def debug():
 
     print(locals())
 
+class Routines(object):
+    """
+    Wrapper for custom routine functions.
+    Just coz Swag.
+    """
+
+    @staticmethod
+    def sep_9_1242(x, y, z):
+        """
+        This method creates a feature in Four stages:
+        1. Create overlapping chunks of the x, y, z axis data, 16 length.
+        2. Fit individual axes on `y = aSin(bx + c) + d`.
+        3. Create a 3x4 matrix, with each row having: [a, b, c, d].
+        4. Remove column with parameter `d` (vertical shift) to get a 3x3 matrix.
+        5. Yield the eigenvalue of the matrix - feature.
+
+        Args:
+            x (list): x axis probe data.
+            y (list): y axis probe data.
+            z (list): z axis probe data.
+
+        Returns:
+            (list): 3D Feature Vector
+        """
+
+        def func(x, a, b, c, d):
+            #: The fit-function
+            #: y = aSin(bx + c) + d
+            return a * np.sin(b * x + c) + d
+
+        #: Overlapped x, y, and z axis data.
+        #  Data length -> 16
+        x_o = zip(*[x[_:] for _ in range(16)])
+        y_o = zip(*[y[_:] for _ in range(16)])
+        z_o = zip(*[z[_:] for _ in range(16)])
+
+        #: Gathers row wise 
+        row = zip(x_o, y_o, z_o)
+
+        for val_set in row:
+            ftr = []
+            for col in val_set:
+                #: Curve fit each column to get the period, phase shift, vertical
+                #: shift, and amplitude.
+                try:
+                    #: if we find optimal fit, then append.
+                    popt = Helper.curve_fit(func, col)
+                    ftr.append(popt)
+                except RuntimeError:
+                    #: Let it be (TM)
+                    #: To keep the structure of the `ftr` intact
+                    #  we do this stupid hack.
+                    ftr.append([0, 0, 0, 0])
+
+            #: Yield a single feature, combining all of the above
+            ftr_cmb = zip(*ftr)
+
+            ampl  = next(ftr_cmb)   # Amplitude
+            phase = next(ftr_cmb)   # Phase
+            ph_sh = next(ftr_cmb)   # Phase Shift
+            ve_sh = next(ftr_cmb)   # Vertical Shift
+
+            eig_val, eig_vec = LA.eig([ampl, phase, ph_sh])
+
+            yield [np.absolute(_) for _ in eig_val]
+
+
 @main.command()
 @click.argument('csv', type = click.File('r'))
 def scratch(csv):
@@ -373,56 +440,6 @@ def scratch(csv):
     x = [_['x'] for _ in row]
     y = [_['y'] for _ in row]
     z = [_['z'] for _ in row]
-
-    def func(x, a, b, c, d):
-        #: The fit-function
-        #: y = aSin(bx + c) + d
-        return a * np.sin(b * x + c) + d
-
-    def mean(*args):
-        #: Finds the mean of arguments
-        return sum(args)/len(args)
-
-    #: Overlapped x, y, and z axis data.
-    #  Data length -> 16
-    x_o = zip(*[x[_:] for _ in range(16)])
-    y_o = zip(*[y[_:] for _ in range(16)])
-    z_o = zip(*[z[_:] for _ in range(16)])
-
-    #: Gathers row wise 
-    row = zip(x_o, y_o, z_o)
-
-    for val_set in row:
-        ftr = []
-        for col in val_set:
-            #: Curve fit each column to get the period, phase shift, vertical
-            #: shift, and amplitude.
-            popt = Helper.curve_fit(func, col)
-            ftr.append(popt)
-
-        #: Yield a single feature, combining all of the above
-        ftr_cmb = zip(*ftr)
-
-        ampl  = next(ftr_cmb)   # Amplitude
-        phase = next(ftr_cmb)   # Phase
-        ph_sh = next(ftr_cmb)   # Phase Shift
-        ve_sh = next(ftr_cmb)   # Vertical Shift
-
-        eig_val, eig_vec = LA.eig([ampl, phase, ph_sh])
-
-        print([np.absolute(_) for _ in eig_val])
-
-        #break
-    """
-    for i in a:
-        popt, pcov = curve_fit(func, list(range(len(i))), i)
-        plt.plot(i)
-        fitted = [func(_, *popt) for _ in range(len(i))]
-        plt.plot(fitted)
-        plt.ylim((-5, 5))
-        plt.show()
-    pass
-    """
 
 if __name__ == "__main__":
     main()
