315fb38843006516563166e57ac0947f29627510
Prashant Sinha <prashant@ducic.ac.in>
2015-09-25 20:39:17 +0530

    iPhone App.

---
+++ Prototypes/Curvy IMU/Curvy IMU/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Curvy IMU
+//
+//  Created by Prashant Sinha on 25/09/15.
+//  Copyright © 2015 Prashant Sinha. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Prototypes/Curvy IMU/Curvy IMU/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Curvy IMU
+//
+//  Created by Prashant Sinha on 25/09/15.
+//  Copyright © 2015 Prashant Sinha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
